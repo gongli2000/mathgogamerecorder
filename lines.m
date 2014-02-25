979e7a511ce993351090d1bb29966425bc829521
gongli2000 <larrye2000@gmail.com>
2014-02-24 19:01:24 -0800

    move line functions

--- lines.m
+++ lines.m
@@ -1,5 +1,37 @@
 (* ::Package:: *)
 
+computePoint[{{{p1_,p2_},{p3_,p4_}},{t_,s_}}]:= t*p1+(1-t)*p2;
+
+
+getBoardBoundaryLines[boardimage_]:=Module[{k,aa,board,src,boundaryimage},
+	src=ColorConvert[boardimage,"Grayscale"];
+	k = MorphologicalComponents[src,.5];
+	board = SelectComponents[k,"Area",-1]//Colorize;
+	aa = MorphologicalPerimeter[board];
+	boundaryimage=SelectComponents[MorphologicalComponents[aa],"Length",-1]// Colorize;
+    ImageLines[boundaryimage]
+
+]
+
+geoTransform[img_,boundingpoly_]:=Module[{nx,ny,pts},
+   {nx,ny}=img//ImageDimensions;
+   pts = {{0,0},{nx,0},{nx,ny},{0,ny}};
+ FindGeometricTransform[pts,boundingpoly]]
+
+
+findBoundaryVertices[lines_]:=Module[{segments,intersections,pointsraw},
+	segments = Subsets[lines,{2}];
+	intersections  =Map[intersectSegments,segments];
+    points = computePoint/@ Select[intersections,inUnitInterval[#[[2]][[1]]] && inUnitInterval[#[[2]][[2]]]&];
+    Map[points[[#]]&, ConvexHull[points]]
+]
+
+getBoardBoundingPolygon[img_]:=Module[{},
+   img // getBoardBoundaryLines // findBoundaryVertices 
+]
+
+
+
 getBoardLines[rectImage_]:= Module[{},
    src=ColorConvert[rectImage,"Grayscale"];
    white=Closing[src,DiskMatrix[5]];
--- utils.m
+++ utils.m
@@ -21,47 +21,13 @@ getfile[filename_]:=ColorConvert[Import[filename],"Grayscale"];
 
 getfileColor[filename_]:=Import[filename];
 
-getBoardLines[rectImage_]:=
-  Module[{src,white,srcAdjusted,components,largestComponent},
-   src=ColorConvert[rectImage,"Grayscale"];
-   white=Closing[src,DiskMatrix[5]];
-   srcAdjusted=Image[ImageData[src]/ImageData[white]]
-   components=ComponentMeasurements[ColorNegate@Binarize[srcAdjusted],{"ConvexArea","Mask"}][[All,2]];
-   largestComponent=Image[SortBy[components,First][[-1,2]]];
-   ImageLines[EdgeDetect[largestComponent]]
-]
+
 
 getNImagesInDir[dir_,n_]:= Module[{filenames},
    SetDirectory[dir];
    SortBy[FileNames["*.JPG"],ToExpression[StringCases[#,RegularExpression["\\d+"]][[1]]]&] // Take[#,n]& // Map[getfileColor[#]&,#]&]
 
-getBoardBoundaryLines[boardimage_]:=Module[{k,aa,board,src,boundaryimage},
-	src=ColorConvert[boardimage,"Grayscale"];
-	k = MorphologicalComponents[src,.5];
-	board = SelectComponents[k,"Area",-1]//Colorize;
-	aa = MorphologicalPerimeter[board];
-	boundaryimage=SelectComponents[MorphologicalComponents[aa],"Length",-1]// Colorize;
-    ImageLines[boundaryimage]
 
-]
-
-geoTransform[img_,boundingpoly_]:=Module[{nx,ny,pts},
-   {nx,ny}=img//ImageDimensions;
-   pts = {{0,0},{nx,0},{nx,ny},{0,ny}};
- FindGeometricTransform[pts,boundingpoly]]
-
-computePoint[{{{p1_,p2_},{p3_,p4_}},{t_,s_}}]:= t*p1+(1-t)*p2;
-
-findBoundaryVertices[lines_]:=Module[{segments,intersections,pointsraw},
-	segments = Subsets[lines,{2}];
-	intersections  =Map[intersectSegments,segments];
-    points = computePoint/@ Select[intersections,inUnitInterval[#[[2]][[1]]] && inUnitInterval[#[[2]][[2]]]&];
-    Map[points[[#]]&, ConvexHull[points]]
-]
-
-getBoardBoundingPolygon[img_]:=Module[{},
-   img // getBoardBoundaryLines // findBoundaryVertices 
-]
 
 maskBoardImage[img_,boardBoundingPoly_]:=Module[{},
 	{nx,ny}=ImageDimensions[img];
